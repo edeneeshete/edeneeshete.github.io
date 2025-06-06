4b4a0046ad36e993a377bebc73c779c6de933dd8
edeneeshete <edeneeshete@gmail.com>
2025-06-06 15:55:49 +1000

    times

--- class-changes.js
+++ class-changes.js
@@ -1,11 +1,11 @@
 const classChanges = [
-  { time: 3, target: ".text-red", add: "textcenter" },
-  { time: 9, target: ".changeme", remove: "show" },
-  { time: 9, target: ".changeme", add: "hidden" },
+  { time: 10, target: ".paraonetext", add: "text-red" },
+  { time: 15, target: ".paraonetext", remove: "text-red" },
+  { time: 67, target: ".largetext", add: "text-blue" },
 
-  { time: 9, target: ".changeme2", add: "show" },
-  { time: 16, target: ".changeme2", remove: "show" },
-  { time: 16, target: ".changeme2", add: "hidden" },
+  { time: 80, target: ".paraonetext", add: "text-blue" },
+  { time: 82, target: ".paraonetext", remove: "text-blue" },
+  { time: 96, target: ".bg-red", add: "bg-blue" },
 
   { time: 16, target: ".changeme3", add: "show" },
   { time: 28, target: ".changeme3", remove: "show" },
