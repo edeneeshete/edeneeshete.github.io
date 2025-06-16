67bc7bd61531b8f60bc81e0a0880f73bf119c426
edeneeshete <edeneeshete@gmail.com>
2025-06-16 22:33:18 +1000

    final edits

--- class-changes.js
+++ class-changes.js
@@ -1,13 +1,21 @@
 const classChanges = [
   { time: 10, target: ".paraonetext", add: "text-red" },
   { time: 15, target: ".paraonetext", remove: "text-red" },
-  { time: 67, target: ".", add: "" },
+  { time: 10, target: ".paraonetext", add: "bg-white" },
+  { time: 15, target: ".paraonetext", remove: "bg-white" },
+  
+  { time: 95, target: ".special", add: "text-blue" },
+  { time: 96, target: ".special", remove: "text-blue" },
+  { time: 97, target: ".special", add: "text-blue" },
+  { time: 98, target: ".special", remove: "text-blue" },
+  { time: 99, target: ".special", add: "text-blue" },
+  { time: 100, target: ".special", remove: "text-blue" },
+  { time: 101, target: ".special", add: "text-blue" },
+  { time: 102, target: ".special", remove: "text-blue" },
 
-  { time: 80, target: ".paraonetext", add: "text-blue" },
-  { time: 82, target: ".paraonetext", remove: "text-blue" },
-  { time: 96, target: ".bg-red", add: "bg-blue" },
-
-  { time: 16, target: ".changeme3", add: "show" },
-  { time: 28, target: ".changeme3", remove: "show" },
-  { time: 28, target: ".changeme3", add: "hidden" }
-];
+  
+  { time: 119, target: ".paraonetext", add: "imgborder" },
+  { time: 155, target: ".paraonetext", remove: "imgborder" },
+  { time: 464, target: ".special", add: "imgborder" },
+  { time: 487, target: ".special", remove: "imgborder" },
+]
